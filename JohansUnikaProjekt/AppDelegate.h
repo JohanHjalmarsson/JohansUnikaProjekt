6a49239c440e12efc8c55fc143206c496bc5d317
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-01-12 14:39:06 +0100

    Initial Commit

---
+++ JohansUnikaProjekt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JohansUnikaProjekt
+//
+//  Created by Johan Hjalmarsson on 2018-01-12.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
